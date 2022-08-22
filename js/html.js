6a8d1507c5c0bddbc342024bf8c79f95c5d875fb
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-08-22 14:12:32 +0600

    player section done

--- js/html.js
+++ js/html.js
@@ -3,7 +3,7 @@
 const card = (player)=>{
     const div = document.createElement('div');
     div.innerHTML = `
-    <div class="max-w-sm rounded overflow-hidden shadow-lg ">
+    <div class="max-w-sm rounded overflow-hidden shadow-lg border-solid">
     <img class="w-full" src="${player.imgUrl}" alt="Sunset in the mountains">
     <div class="px-6 py-4">
       <div class="font-bold text-xl mb-2">${player.name}</div>
