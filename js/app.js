a932cc16da1ce81b74effebfae4dfa5dd3a9b897
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-08-23 12:28:05 +0600

    everyting is okk its time to deploay and submit

--- js/app.js
+++ js/app.js
@@ -4,84 +4,109 @@ const totalCost = document.getElementById('total-cost')
 
 let count = 0
 
-for(const player of players){
+// load data
+
+for (const player of players) {
    const cart = card(player)
+}
+
+// soow error or success Msg
+
+// show error ort success  massage function
+
+const showMassage = (text, status) => {
+   const div = document.createElement('div');
+
+   div.className = `toast-message toast-message-slide-in bg-${status}`;
+   div.innerText = text;
+   document.body.appendChild(div)
+
+   div.addEventListener('click', () => {
+      div.classList.remove('toast-message-slide-in');
+      div.classList.add('toast-message-slide-out');
+
+   })
+   setTimeout(() => {
+      div.remove();
+   }, 2000)
 }
 
 
 
 // add to cart function
 
-const addToCart = (event)=>{
-   
-  count = count + 1
+const addToCart = (event) => {
+
+   count = count + 1
 
    const playerName = event.target.parentNode.children[1].innerText
-   
-   
- const li = document.createElement('li')
+
+
+   const li = document.createElement('li')
    li.innerHTML = `
    <li class="font-bold text-xl py-3 border-solid">${count}.  ${playerName}</li>
    
    `
-//   check condition
+   //   check condition
 
-if (count <=5) {
-   document.getElementById('players-list').appendChild(li)
-  
-const disableBtn = event.target.setAttribute('disabled', '');
- 
-const setClass = event.target.classList.add("disable-btn")
-}else{
-  alert('maxium 5 player is added')
+   if (count <= 5) {
+      document.getElementById('players-list').appendChild(li)
+
+      const disableBtn = event.target.setAttribute('disabled', '');
+      event.target.innerText = 'Added'
+      showMassage('successfully Added player', 'success')
+
+
+      const setClass = event.target.classList.add("disable-btn")
+   } else {
+      showMassage('you Can Add Maximum 5 player', 'danger')
+   }
+
+
+
+
 }
 
-   
+
+// reusable function parse into number
 
 
-}
-
-
-
-// parse into number
-
-// functions
 
 const parseIntoNumber = (id) => {
    const strValue = document.getElementById(id).value
    const numberValue = parseFloat(strValue)
 
    if (isNaN(numberValue)) {
-       return 0
+      return 0
    } else {
-       return numberValue
+      return numberValue
    }
-   
+
 }
 
-const  totalPlayerCost = ()=>{
-   const totalPlayerCost = parseIntoNumber('per-player')*count
+const totalPlayerCost = () => {
+   const totalPlayerCost = parseIntoNumber('per-player') * count
    document.getElementById('player-expence').value = totalPlayerCost
 
-   
-   
+
+
 }
 
-
+// total calculation cost and added to dom
 
 const calcTotal = () => {
 
    const totalExp = parseIntoNumber("player-expence") + parseIntoNumber("manager") + parseIntoNumber("couch")
    totalCost.innerText = totalExp;
 
-  
 
-if (isNaN(totalBalanceNumber)) {
-  alert('its be a number')
-   
-}else{
-   return totalBalanceNumber
-}
+
+   if (isNaN(totalBalanceNumber)) {
+      showMassage('its be a Number')
+
+   } else {
+      return totalBalanceNumber
+   }
 
 }
 
